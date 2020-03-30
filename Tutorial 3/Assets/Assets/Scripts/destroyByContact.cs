266dbf0e597083aff7251ea414aa17f4e4145f26
Ianka364 <ianka@knights.ucf.edu>
2020-03-29 23:38:16 -0400

    Tutorial 3

---
+++ Tutorial 3/Assets/Assets/Scripts/destroyByContact.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    public GameController gameController;
+
+    void Start ()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject!=null)
+        {
+            gameController = gameController.GetComponent<GameController>();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+        gameController.AddScore(scoreValue);
+    }
+}
